beaabfb8cd7153abf3691a1eef9fda493d632233
Alberto Barrago <albertobarrago@gmail.com>
2024-09-10 16:52:03 +0200

    Added some classes and logic

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ screen.tracer(0)
 
 snake = Snake()
 
-
 while GAME_IS_ON:
     screen.update()
     time.sleep(0.1)
-
     snake.move_forward()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.right, "Right")
+    screen.onkey(snake.left, "Left")
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        GAME_IS_ON = False
+        print("Game Over")
 
-screen.listen()
 screen.exitonclick()
---
+++ score.py
@@ -0,0 +1,4 @@
+""" Score module for snake game """
+from turtle import Turtle
+
+
